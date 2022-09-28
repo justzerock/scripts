218ec22ab4e43d5fad0da247eefafaa45a04cc4f
justzerock <liu.zerock@gmail.com>
2022-09-28 23:23:41 +0800

    完善

--- stash/bwg-count.js
+++ stash/bwg-count.js
@@ -4,11 +4,33 @@ $httpClient.get($argument, (error, response, data) => {
     console.log(error)
   } else {
     let bwg = JSON.parse(data)
+
+    let multiplier = data.monthly_data_multiplier
+    let plan_monthly_data = toGB(data.plan_monthly_data, multiplier)
+    let data_counter = toGB(data.data_counter, multiplier)
+    let data_next_reset = timestamp(data.data_next_reset)
+    let content = `
+    流量：已使用 ${data_counter}，共 ${plan_monthly_data}
+    重置日期：${data_next_reset}
+    `
+
     $done({
-      title: bwg.node_location + 'haha',
-      content: bwg.email,
+      title: bwg.node_location,
+      content: content,
       backgroundColor: "#663399",
       icon: "network",
     })
   }
 })
+
+function toGB(bit, multi) {
+  let GB = bit*multi/(1024*1024*1024)
+  return GB.toFixed(2) + 'GB'
+}
+
+function toDate(timestamp) {
+  let date = new Date(timestamp*1000)
+  let month = date.getMonth() + 1
+  let day = date.getDate()
+  return month + '月' + day + '日'
+}
