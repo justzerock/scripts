4bc01e7939d1282262d5dbbd5eaf750b25f3fa7b
justzerock <liu.zerock@gmail.com>
2022-09-29 00:10:37 +0800

    修改

--- stash/bwg-count.js
+++ stash/bwg-count.js
@@ -4,15 +4,17 @@ $httpClient.get($argument, (error, response, data) => {
     console.log(error)
   } else {
     let bwg = JSON.parse(data)
+    let date = new Date()
+    let time = date.getHours() + ':' + date.getMinutes()
     let multiplier = bwg.monthly_data_multiplier
     let plan_monthly_data = toGB(bwg.plan_monthly_data, multiplier)
     let data_counter = toGB(bwg.data_counter, multiplier)
     let data_next_reset = toDate(bwg.data_next_reset)
-    let content = `🌈 已使用 ${data_counter}/${plan_monthly_data}GB
+    let content = `🌈 已使用 ${data_counter} / ${plan_monthly_data}GB
     剩余 ${(plan_monthly_data - data_counter).toFixed(2)}GB
 
-    🎯 将于 ${data_next_reset} 重置`
-    
+    更新于 ${time}，流量将于 ${data_next_reset} 重置`
+
     $done({
       title: '🇺🇸 ' + bwg.node_location,
       content: content,
