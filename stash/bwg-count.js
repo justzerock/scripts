684631767709168468bf42154e5d1c8ff48183ae
justzerock <liu.zerock@gmail.com>
2022-09-28 18:31:49 +0800

    测试

--- stash/bwg-count.js
+++ stash/bwg-count.js
@@ -1,33 +1,13 @@
 
-$httpClient.get("https://api.64clouds.com/v1/getServiceInfo?veid=1172197&api_key=private_RJT1Ik0L10x759KFDbu2ezB8", (error, response, data) => {
+$httpClient.get($argument, (error, response, data) => {
   if (error) {
     console.log(error)
   } else {
-    let multiplier = data.monthly_data_multiplier
-    let plan_monthly_data = toGB(data.plan_monthly_data, multiplier)
-    let data_counter = toGB(data.data_counter, multiplier)
-    let data_next_reset = timestamp(data.data_next_reset)
-    let content = `
-    流量：已使用 ${data_counter}，共 ${plan_monthly_data}
-    重置日期：${data_next_reset}
-    `
     $done({
       title: data.node_location,
-      content: content,
+      content: data.data_counter*multi/(1024*1024*1024),
       backgroundColor: "#663399",
       icon: "network",
     })
   }
 })
-
-function toGB(bit, multi) {
-  let GB = bit*multi/(1024*1024*1024)
-  return GB.toFixed(2) + 'GB'
-}
-
-function toDate(timestamp) {
-  let date = new Date(timestamp*1000)
-  let month = date.getMonth() + 1
-  let day = date.getDate()
-  return month + '月' + day + '日'
-}
